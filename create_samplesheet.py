56649374b57c91ddc6ee7b7897ea80ef260065fa
nbtm-sh <nathan@nbti.net>
2024-11-14 15:18:38 +1100

    breaking chnage:wq

--- create_samplesheet.py
+++ create_samplesheet.py
@@ -18,7 +18,7 @@ class Sample:
         self.path = path
         self.data = data
 
-def sanitize_input(input_str, disallowed_chars = [',', ' ', '<', '>', '.', "'", '"', ';', ':'], replcement='_'):
+def sanitize_input(input_str, disallowed_chars = [' ', '<', '>', '.', "'", '"', ';', ':'], replcement='_'):
     for i in disallowed_chars:
         input_str = input_str.replace(i, '_')
     return input_str
