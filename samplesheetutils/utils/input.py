6a2882c6be3aace7ee59af3d1d9e7c99833ab2c8
nbtm-sh <nathan@nbti.net>
2024-12-05 14:06:31 +1100

    fix(sani): Added brackets to disallowed characters

--- samplesheetutils/utils/input.py
+++ samplesheetutils/utils/input.py
@@ -1,4 +1,4 @@
-def sanitize_input(input_str, disallowed_chars = [',', ' ', '<', '>', '.', "'", '"', ';', ':'], replcement='_'):
+def sanitize_input(input_str, disallowed_chars = [',', ' ', '<', '>', '.', "'", '"', ';', ':', '(', ')'], replcement='_'):
     for i in disallowed_chars:
         input_str = input_str.replace(i, '_')
     return input_str
